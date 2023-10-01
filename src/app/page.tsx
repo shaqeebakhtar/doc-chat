0abce9d448aec5f9a5a7d3147814e78cda0ac2cd
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-10-01 18:46:24 +0530

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,8 @@ export default function Home() {
               Start chatting in minutes
             </h2>
             <p className="mt-4 text-lg text-gray-600">
-              Chatting to your PDF files has never been easier than with Quill.
+              Chatting to your PDF files has never been easier than with
+              DocChat.
             </p>
           </div>
         </div>
@@ -117,7 +118,7 @@ export default function Home() {
                 Start asking questions
               </span>
               <span className="mt-2 text-zinc-700">
-                It&apos;s that simple. Try out Quill today - it really takes
+                It&apos;s that simple. Try out DocChat today - it really takes
                 less than a minute.
               </span>
             </div>
